12b029b99ae27919ef9dff87dcb3b7c9855c106c
Steven Hsu <sthsu@ucsd.edu>
2024-10-17 17:42:15 -0700

    removed messaging feature

--- hackathon/pages/api/rooms/index.ts
+++ hackathon/pages/api/rooms/index.ts
@@ -1,9 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import dbConnect from "../lib/dbConnect";
+import dbConnect from "../../lib/dbConnect";
 import { RtcTokenBuilder, RtcRole } from "agora-access-token";
 import { RtmTokenBuilder, RtmRole } from "agora-access-token";
-import Room from "../models/Room";
+import Room from "../../models/Room";
 
 type Room = {
   status: String;
@@ -55,16 +54,43 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const { method, query } = req;
-  const roomId = query.roomId as string;
+  const userId = query.userId as string;
 
   await dbConnect();
 
   switch (method) {
-    case "PUT":
-      await Room.findByIdAndUpdate(roomId, {
+    case "GET":
+      try {
+        const rooms = await Room.aggregate([
+          { $match: { status: "waiting" } },
+          { $sample: { size: 1 } },
+        ]);
+        if (rooms.length > 0) {
+          const roomId = rooms[0]._id.toString();
+          await Room.findByIdAndUpdate(roomId, {
+            status: "chatting",
+          });
+          res.status(200).json({
+            rooms,
+            rtcToken: getRtcToken(roomId, userId),
+            rtmToken: getRtmToken(userId),
+          });
+        } else {
+          res.status(200).json({ rooms: [], token: null });
+        }
+      } catch (error) {
+        res.status(400).json((error as any).message);
+      }
+      break;
+    case "POST":
+      const room = await Room.create({
         status: "waiting",
       });
-      res.status(200).json("success");
+      res.status(200).json({
+        room,
+        rtcToken: getRtcToken(room._id.toString(), userId),
+        rtmToken: getRtmToken(userId),
+      });
       break;
     default:
       res.status(400).json("no method for this endpoint");
