2cf6981a62ad2d322546e814c1307fb046c85a04
emilioece <emailemilioee@gmail.com>
2024-10-18 11:24:08 -0700

    other

--- hackathon/next.config.mjs
+++ hackathon/next.config.mjs
@@ -1,6 +1,8 @@
+import withImages from 'next-images';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Add any other Next.js config options here
 };
 
-export default nextConfig;
+export default withImages(nextConfig);
